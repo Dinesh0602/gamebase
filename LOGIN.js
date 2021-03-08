8ff2846233afa26058917713dc8edcb0e121a490
Dinesh Buruboyina <buruboyinadinesh@gmail.com>
2021-03-08 23:03:16 +0530

    Changes to be committed:

--- LOGIN.js
+++ LOGIN.js
@@ -20,26 +20,25 @@ var firebaseConfig = {
      
       var email = document.getElementById("email_field").value;
       var password = document.getElementById("password_field").value;
-      firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
+      firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(user => {
+       console.log(user);
+        
+        alert("Welcome " +email);
+        
+        window.location.href = "Home.html";
+      }).catch(function(error) {
+        
         var errorCode = error.code;
         var errorMessage = error.message;
     
         window.alert("Error : " + errorMessage);
     
-        // ...
+        
       });
     
     }
-//       const promise = firebase.auth().signInWithEmailAndPassword(email, password);
-//  promise.catch(e => alert(e.message));
-  // .catch((error) => {
-  //   var errorCode = error.code;
-  //   var errorMessage = error.message;
-  //   alert("ERROR: " +errorMessage);
-  //   alert("ERROR: " +errorCode);
-  // });
-  //   }
+
     
     function signUp()
     {
@@ -47,32 +46,21 @@ var firebaseConfig = {
       const email = document.getElementById("email_field").value;
       const password = document.getElementById("password_field").value;
     firebase.auth().createUserWithEmailAndPassword(email, password)
-    // firebase.auth().signInWithEmailAndPassword(email, password)
+    
     .catch((error) => {
       var errorCode = error.code;
       var errorMessage = error.message;
       alert("ERROR: " +errorCode);
       alert("ERROR: " +errorMessage)
-      // ..
+      
     });
   }
-  const auth = firebase.auth();
-  auth.onAuthStateChanged(function(user){
-    
-    if(user){
-    document.getElementsById("Home").style.display = "block";
-    document.getElementsById("Login").style.display = "none";
-
-
-    }else{
-      
-      document.getElementsById("Home").style.display = "none";
-      document.getElementsById("Login").style.display = "block";
-     }
-    });
+ 
    function signOut(){
       
       firebase.auth().signOut();
+      alert("Signed out");
+      window.location.href = "Login.html";
       
       
     }
